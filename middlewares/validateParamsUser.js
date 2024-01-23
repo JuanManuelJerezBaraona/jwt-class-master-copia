59888addd750d34248e9f64f2b6a6045026aef1d
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-23 17:47:47 -0300

    first commit

---
+++ middlewares/validateParamsUser.js
@@ -0,0 +1,28 @@
+// const validateParametersUser = (req, res, next) => {
+//     const { user } = req.body;
+//     if (!user.email || !user.password || !user.nombre || !user.apellido) {
+//         return res.status(400).json({ error: "el email, password, nombre y apellido deben estar presentes" });
+//     }
+//     next();
+// }
+
+// export { validateParametersUser };
+
+import { check, validationResult } from 'express-validator';
+
+const validateParametersUser = [
+    check('user.email').isEmail().withMessage('Debe ser un correo electrónico válido'),
+    check('user.password').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres'),
+    check('user.nombre').notEmpty().withMessage('El nombre es requerido'),
+    check('user.apellido').notEmpty().withMessage('El apellido es requerido'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
+
+export { validateParametersUser };
